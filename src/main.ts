e0f6e99bc68769ee1231d5fa146d2157e5d1a565
Salam <sharqiyem@yandex.ru>
2023-08-01 23:25:17 +0800

    fix port

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 
   if (module.hot) {
     module.hot.accept();
